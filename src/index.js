547b11c9c2f869868af77df1e832531db981b497
Brennaser <sermanbc@dukes.jmu.edu>
2022-06-29 11:14:45 -0400

    Clean up MenuBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
 import ExperimentalSetup from './Components/routes/ExperimentalSetup';
 
 
