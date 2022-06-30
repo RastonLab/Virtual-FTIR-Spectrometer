14c98ce5982a6fb7200c4a3d575994526762d0d8
Brennaser <sermanbc@dukes.jmu.edu>
2022-06-30 10:19:24 -0400

    SVG Displayed

---
+++ src/Components/SVGComponents.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { ftirParts } from "./SVGLibrary";
+// import Tooltip from "./Tooltip";
+
+export default function SVGComponent({part, style, click}) { // Style is the intial stroke
+    const [toggled, setToggled] = useState(false);
+
+    const handleClick = (ev) => {
+        setToggled(!toggled)
+    }
+
+    const Component = ftirParts[part];
+
+    return (<div>
+                <Component className='svg' onClick={click ?? handleClick} />
+                {/* <Tooltip 
+                    header={toolTips[part].header}
+                    text={toolTips[part].text}
+                    visible={toolTips[part]?.visible ?? toggled}
+                /> */}
+            </div>
+    );
+    // SVG File Prep: put them through SVGOMG to strip any inkscape specific tags that would break React
+    // In the SVG files, you must remove the stroke and stroke-height of the element you want outlined and you must remove the  height and width from the very first line to get the SVG to use the viewbox and scale properly
+}
---
+++ src/Components/SVGLibrary.js
@@ -0,0 +1,5 @@
+import {ReactComponent as FTIRSVG} from './svgs/FTIR.svg';
+
+export const ftirParts = {
+    'ftir': FTIRSVG
+}
