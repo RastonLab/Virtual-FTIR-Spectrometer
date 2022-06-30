f92e584c88f017e4d7183605f4945d04dc878654
Brennaser <sermanbc@dukes.jmu.edu>
2022-06-30 09:40:20 -0400

    Homepage Button Added

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,5 +1,6 @@
 import { menuItems } from "../menuItems";
 import MenuItems from "./MenuItems";
+import { Link } from "react-router-dom";
 import {ReactComponent as RLLogo} from '../RastonLab-Logo-Full-Rainbow-Draft.svg';
 
 export default function MenuBar() {
@@ -8,8 +9,8 @@ export default function MenuBar() {
 
     return (
         <div className="nav-area">
-            <RLLogo className='logo' width={'58'} viewBox='-10 -158 100 400'/>
-            <p className='logo'>The Raston Lab: FTIR</p>
+            <RLLogo className='logo' width={'58'} viewBox='-10 -158 100 400'/> 
+            <Link className='logo' to='/Virtual-FTIR-Spectrometer' >The Raston Lab: FTIR</Link>
             <nav>
                 <ul className="menus">
                     {menuItems.map((menu, index) => {
