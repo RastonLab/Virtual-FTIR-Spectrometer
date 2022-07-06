07106202f9dc44ccd6ba7d359f2aa7712fe21a7a
VanFossen <nvanfossen@gmail.com>
2022-07-06 10:58:01 -0400

    formatting

--- src/Components/routes/ExperimentalSetup.jsx
+++ src/Components/routes/ExperimentalSetup.jsx
@@ -1,5 +1,5 @@
-import MenuBar from '../MenuBar';
-import '../../style/ExperimentalSetup.css';
+import MenuBar from "../MenuBar";
+import "../../style/ExperimentalSetup.css";
 // import ElementToggle from '../ElementToggle';
 
 import { useState } from "react";
@@ -13,56 +13,56 @@ import Error from "../Error";
 import Plotly from "../graphs/Plotly";
 
 export default function ExperimentalSetup() {
-    const [data, setData] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-  
-    /**
-     * Default Radis App Values
-     *   species: [{ molecule: "CO", mole_fraction: 0.1 }]
-     *   min_wavenumber_range: 1900
-     *   max_wavenumber_range: 2300
-     *   tgas: 300
-     *   tvib: null
-     *   trot: null
-     *   pressure: 1.01325
-     *   path_length: 1
-     *   simulate_slit: false
-     *   mode: "absorbance"
-     *   database: "hitran"
-     */
-    const [params, setParams] = useState({
-      species: [{ molecule: "CO", mole_fraction: 1 }],
-      min_wavenumber_range: 1900,
-      max_wavenumber_range: 2300,
-      tgas: 294.15,
-      tvib: null,
-      trot: null,
-      pressure: 0.0001,
-      path_length: 10,
-      simulate_slit: false,
-      mode: "transmittance_noslit",
-      database: "hitran",
-    });
-  
-    return (
-        <div className='App'>
-            <MenuBar />
-            <div className="experiment-setup">
-                <Form params={params} setParams={setParams} />
-                <Fetch
-                params={params}
-                setData={setData}
-                setLoading={setLoading}
-                setError={setError}
-                />
-                {loading && <Spinner />}
-                {error && <Error />}
-        
-                {/* {!loading && <Dygraphs data={data} />} */}
-                {/* {!loading && <ChartJS data={data} />} */}
-                {!loading && <Plotly data={data} params={params} />}
-            </div>
-        </div>
-        );
-  }
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  /**
+   * Default Radis App Values
+   *   species: [{ molecule: "CO", mole_fraction: 0.1 }]
+   *   min_wavenumber_range: 1900
+   *   max_wavenumber_range: 2300
+   *   tgas: 300
+   *   tvib: null
+   *   trot: null
+   *   pressure: 1.01325
+   *   path_length: 1
+   *   simulate_slit: false
+   *   mode: "absorbance"
+   *   database: "hitran"
+   */
+  const [params, setParams] = useState({
+    species: [{ molecule: "CO", mole_fraction: 1 }],
+    min_wavenumber_range: 1900,
+    max_wavenumber_range: 2300,
+    tgas: 294.15,
+    tvib: null,
+    trot: null,
+    pressure: 0.0001,
+    path_length: 10,
+    simulate_slit: false,
+    mode: "transmittance_noslit",
+    database: "hitran",
+  });
+
+  return (
+    <div className="App">
+      <MenuBar />
+      <div className="experiment-setup">
+        <Form params={params} setParams={setParams} />
+        <Fetch
+          params={params}
+          setData={setData}
+          setLoading={setLoading}
+          setError={setError}
+        />
+        {loading && <Spinner />}
+        {error && <Error />}
+
+        {/* {!loading && <Dygraphs data={data} />} */}
+        {/* {!loading && <ChartJS data={data} />} */}
+        {!loading && <Plotly data={data} params={params} />}
+      </div>
+    </div>
+  );
+}
