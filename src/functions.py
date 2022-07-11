351b83af1526e27c4429de02d3c44dd2134bb79a
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-11 13:07:51 -0400

    Fixed Functions

--- src/functions.py
+++ src/functions.py
@@ -58,8 +58,8 @@ def CaF2(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = (0.93091) / (1 + (11.12929/ x ) ** -12.43933 ) ** 4.32574
+        y = float(originalData[x])
+        datapoint = (0.93091) / (1 + (11.12929/ y ) ** -12.43933 ) ** 4.32574
         newData[x] = datapoint
     
     __exportData(newData)
@@ -74,8 +74,8 @@ def ZnSe(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = (0.71015) / ((1 + (20.99353 / x) ** -19.31355 ) ** 1.44348) + -0.13265 / (2.25051 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 16.75) ** 2) / (2.25051 ** 2))
+        y = float(originalData[x])
+        datapoint = (0.71015) / ((1 + (20.99353 / y) ** -19.31355 ) ** 1.44348) + -0.13265 / (2.25051 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((y - 16.75) ** 2) / (2.25051 ** 2))
 
         newData[x] = datapoint
 
@@ -91,8 +91,8 @@ def sapphire(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = (0.78928) / (1 + (11.9544/ x) ** -12.07226 ) ** 6903.57039
+        y = float(originalData[x])
+        datapoint = (0.78928) / (1 + (11.9544/ y) ** -12.07226 ) ** 6903.57039
         newData[x] = datapoint
     
     __exportData(newData)
@@ -107,8 +107,8 @@ def AR_ZnSe(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = (0.82609) / ((1 + ((34.63971 / x) ** -8.56269)) ** 186.34792) + -0.47 / (0.55* math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 1.47) ** 2) / (0.55 ** 2)) + -0.03456 / (0.4 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 2.88) ** 2) / (0.4 ** 2)) + -0.009 / (0.3 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 6.16) ** 2) / (0.3 ** 2)) + -0.09 / (1 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 16.2) ** 2) / (1 ** 2)) + -0.08 / (1 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 17.4) ** 2) / (1 ** 2)) + 1.12 / (8 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 9.5) ** 2) / (8 ** 2)) + 0.11546 / (2 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 4.9) ** 2) / (2 ** 2)) + 0.21751 / (2 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 2.6) ** 2) / (2 ** 2)) + -0.05 / (0.07 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 0.8) ** 2) / (0.07 ** 2))
+        y = float(originalData[x])
+        datapoint = (0.82609) / ((1 + ((34.63971 / y) ** -8.56269)) ** 186.34792) + -0.47 / (0.55* math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 1.47) ** 2) / (0.55 ** 2)) + -0.03456 / (0.4 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 2.88) ** 2) / (0.4 ** 2)) + -0.009 / (0.3 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 6.16) ** 2) / (0.3 ** 2)) + -0.09 / (1 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 16.2) ** 2) / (1 ** 2)) + -0.08 / (1 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 17.4) ** 2) / (1 ** 2)) + 1.12 / (8 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 9.5) ** 2) / (8 ** 2)) + 0.11546 / (2 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 4.9) ** 2) / (2 ** 2)) + 0.21751 / (2 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 2.6) ** 2) / (2 ** 2)) + -0.05 / (0.07 * math.sqrt(math.pi  / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 0.8) ** 2) / (0.07 ** 2))
 
         newData[x] = datapoint
     
@@ -124,8 +124,8 @@ def AR_CaF2(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = (0.9795) / ((1 + ((18.77617/ x ) ** -6.94246) ) ** 91.98745) + -0.06 / (0.08 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -0.76) ** 2) / (0.08 ** 2))+-0.06 / (0.2 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2)( x -1.06) ** 2/0.20 ** 2) + -0.6 / (3.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2)(( x -4.85) ** 2) / (3.0 ** 2)) + -0.35 / (1.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x - 9.40) ** 2) / (1.00 ** 2)) + 0.05 / (0.8 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -2.60) ** 2) / (0.8 ** 2)) + 0.04 / (0.5 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -7.75) ** 2) / (0.50 ** 2)) + -0.01 / (0.6 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -6.55) ** 2) / (0.6 ** 2)) + 0.01 / (0.5 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -1.82) ** 2) / (0.5 ** 2))
+        y = float(originalData[x])
+        datapoint = (0.9795) / ((1 + ((18.77617/ y ) ** -6.94246) ) ** 91.98745) + -0.06 / (0.08 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y -0.76) ** 2) / (0.08 ** 2))+-0.06 / (0.2 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2)( y -1.06) ** 2/0.20 ** 2) + -0.6 / (3.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2)(( y -4.85) ** 2) / (3.0 ** 2)) + -0.35 / (1.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y - 9.40) ** 2) / (1.00 ** 2)) + 0.05 / (0.8 * math.sqrt(math.pi / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y -2.60) ** 2) / (0.8 ** 2)) + 0.04 / (0.5 * math.sqrt(math.pi / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y -7.75) ** 2) / (0.50 ** 2)) + -0.01 / (0.6 * math.sqrt(math.pi / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y -6.55) ** 2) / (0.6 ** 2)) + 0.01 / (0.5 * math.sqrt(math.pi /(4 * math.log(2)))) * math.eyp(-4 * math.log(2) * (( y -1.82) ** 2) / (0.5 ** 2))
         newData[x] = datapoint
     
     __exportData(newData)
@@ -140,8 +140,8 @@ def InSb(inputFile):
     newData = dict()
 
     for x in originalData:
-        x = float(x)
-        datapoint = 1.97163E11 * (1 / (1 + math.exp( -(x - 5.3939) / 1.6624))) * (1 - 1 / (1 + math.exp( -(x - 5.3939) / 0.11925))) + (3.3 * (10 ** 10)) / (2.44977 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 5) ** 2) / (2.44977 ** 2))
+        y = float(originalData[x])
+        datapoint = 1.97163E11 * (1 / (1 + math.eyp( -(y - 5.3939) / 1.6624))) * (1 - 1 / (1 + math.eyp( -(y - 5.3939) / 0.11925))) + (3.3 * (10 ** 10)) / (2.44977 * math.sqrt(math.pi / (4 * math.log(2)))) * math.eyp(-4 * math.log(2) * ((y - 5) ** 2) / (2.44977 ** 2))
 
         newData[x] = datapoint
     
@@ -163,8 +163,8 @@ def MCT(inputFile):
     
     return __exportData(newData)
 
-# if __name__ == "__main__":
-#     KBr("globar-1900-2300.csv")
+if __name__ == "__main__":
+    KBr("src/globar-1900-2300.csv")
     
 #     # Set up for actual
 #     data = __loadData("outputDataC.csv")
