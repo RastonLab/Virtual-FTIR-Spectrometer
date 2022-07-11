1cc184e6d9002ad2702f4fe2b300bd7091cb4e8f
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-11 10:29:01 -0400

    File I/O and Dictionaries

--- src/functions.py
+++ src/functions.py
@@ -1,98 +1,153 @@
 import math
 
-def __loadData():
-    return
+def __loadData(inputFile):
 
-def __exportData():
-    return
+    file = open(inputFile, 'r').split('\n')
 
-def KBr():
-    originalData = __loadData()
-    newData = []
+    # File not found
+    # May need corrections
+    if not file:
+        return "File not found"
+
+    data = dict()
+
+    for line in file:
+        (key, val) = line.split()
+        data[key] = val
+
+    file.close()
+
+    return data
+
+def __exportData(data):
+
+    # open file and start at the begining
+    file = open("outputDataC.txt", 'w')
+
+    for key in data:
+        line = key + " " + data[key] + "\n"
+        file.write(line)
+
+    return 
+
+def KBr(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.92267) / (1 + (25.66477 / x) ** -12.35159) ** 0.17344
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
+    finalData = __exportData(newData)
     return finalData
 
-def CaF2():
-    originalData = __loadData()
-    newData = []
+def CaF2(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.93091) / (1 + (11.12929/ x ) ** -12.43933 ) ** 4.32574
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def ZnSe():
-    originalData = __loadData()
-    newData = []
+def ZnSe(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.71015) / ((1 + (20.99353 / x) ** -19.31355 ) ** 1.44348) + -0.13265 / (2.25051 * math. math.sqrt(math.math.pi /(4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 16.75) ** 2) / (2.25051 ** 2))
 
-        newData.append(datapoint)
+        newData[x] = datapoint
 
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def sapphire():
-    originalData = __loadData()
-    newData = []
+def sapphire(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.78928) / (1 + (11.9544/ x) ** -12.07226 ) ** 6903.57039
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def AR_ZnSe():
-    originalData = __loadData()
-    newData = []
+def AR_ZnSe(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.82609) / ((1 + ((34.63971 / x) ** -8.56269)) ** 186.34792) + -0.47 / (0.55* math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 1.47) ** 2) / (0.55 ** 2)) + -0.03456 / (0.4 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 2.88) ** 2) / (0.4 ** 2)) + -0.009 / (0.3 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 6.16) ** 2) / (0.3 ** 2)) + -0.09 / (1 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 16.2) ** 2) / (1 ** 2)) + -0.08 / (1 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 17.4) ** 2) / (1 ** 2)) + 1.12 / (8 * math. math.sqrt(math.math.pi /(4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 9.5) ** 2) / (8 ** 2)) + 0.11546 / (2 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 4.9) ** 2) / (2 ** 2)) + 0.21751 / (2 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 2.6) ** 2) / (2 ** 2)) + -0.05 / (0.07 * math. math.sqrt(math.math.pi  / (4 * math.log(2)))) * math.math.exp(-4 * math.log(2) * ((x - 0.8) ** 2) / (0.07 ** 2))
 
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def AR_CaF2():
-    originalData = __loadData()
-    newData = []
+def AR_CaF2(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (0.9795) / ((1 + ((18.77617/ x ) ** -6.94246) ) ** 91.98745) + -0.06 / (0.08 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -0.76) ** 2) / (0.08 ** 2))+-0.06 / (0.2 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2)( x -1.06) ** 2/0.20 ** 2) + -0.6 / (3.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2)(( x -4.85) ** 2) / (3.0 ** 2)) + -0.35 / (1.0 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x - 9.40) ** 2) / (1.00 ** 2)) + 0.05 / (0.8 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -2.60) ** 2) / (0.8 ** 2)) + 0.04 / (0.5 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -7.75) ** 2) / (0.50 ** 2)) + -0.01 / (0.6 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -6.55) ** 2) / (0.6 ** 2)) + 0.01 / (0.5 * math.sqrt(math.pi /(4 * math.log(2)))) * math.exp(-4 * math.log(2) * (( x -1.82) ** 2) / (0.5 ** 2))
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def lnSb():
-    originalData = __loadData()
-    newData = []
+def lnSb(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = 1.97163E11 * (1 / (1 + math.exp( -(x - 5.3939) / 1.6624))) * (1 - 1 / (1 + math.exp( -(x - 5.3939) / 0.11925))) + (3.3 * (10 ** 10)) / (2.44977 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 5) ** 2) / (2.44977 ** 2))
 
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    __exportData(newData)
+    return 
 
-def MCT():
-    originalData = __loadData()
-    newData = []
+def MCT(inputFile):
+    originalData = __loadData(inputFile)
+
+    if originalData == "File not found":
+        return originalData
+
+    newData = dict()
 
     for x in originalData:
         datapoint = (1.98748 * (10 ** 9)) + (2.10252 * (10 ** 10)) * (1 / (1 + math.exp( -(x - 20.15819) / 5.73688))) * (1 - 1 / (1 + math.exp( -(x - 20.15819) / 1.11659))) + (1.3 * (10 ** 9)) / (2 * math.sqrt(math.pi / (4 * math.log(2)))) * math.exp(-4 * math.log(2) * ((x - 18.6) ** 2) / (2 ** 2))
-        newData.append(datapoint)
+        newData[x] = datapoint
     
-    finalData = __exportData()
-    return finalData
+    return __exportData(newData)
