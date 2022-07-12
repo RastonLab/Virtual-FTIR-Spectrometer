c2795d98beb87027ccbfc731a0227a1e88412a74
Ronal Gomez <103782413+ronalgomezjmu@users.noreply.github.com>
2022-07-12 09:37:23 -0400

    added print button

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -8,6 +8,8 @@ import MoleFraction from "./inputs/MoleFraction";
 import PathLength from "./inputs/PathLength";
 import Pressure from "./inputs/Pressure";
 import Tgas from "./inputs/Tgas";
+import Print from "./Print";
+
 
 function Input({ params, setParams }) {
   return (
@@ -29,6 +31,8 @@ function Input({ params, setParams }) {
       <Molecule params={params} setParams={setParams} />
 
       <MoleFraction params={params} setParams={setParams} />
+
+      <Print params={params} setParams={setParams} />
     </div>
   );
 }
---
+++ src/Components/Print.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function fullPage() {
+    return (
+        window.print()
+    );
+  }
+
+function Print () {
+    return (
+        <button id="button" onClick={fullPage}>
+            Print
+        </button>
+    );
+}
+
+export default Print;
