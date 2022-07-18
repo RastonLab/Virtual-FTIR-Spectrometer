88f042938d2ec09b2689133fcc9b8dcbb098e96e
Vanfossen <nvanfossen@gmail.com>
2022-07-17 22:29:06 -0400

    implemented electronics panel, need css work

--- src/routes/InstrumentWindow.jsx
+++ src/routes/InstrumentWindow.jsx
@@ -1,30 +1,38 @@
 import React from "react";
+
 import SVGComponent from "../components/SVGComponents";
+import Electronics from "../components/Electronics";
 
 import "../style/InstrumentWindow.css";
 
 function InstrumentWindow() {
   return (
     <div id="instrument-window">
-      <SVGComponent part="laserbeams" click={() => {}} />
-      <SVGComponent part="sourcebox" click={() => {}} />
-      <SVGComponent part="aperturewheel" />
-      <SVGComponent part="flatrotatablemirror" />
-      <SVGComponent part="globar" />
-      <SVGComponent part="parabolicmirror" />
-      <SVGComponent part="tungsten" />
-      <SVGComponent part="detectionchamber" click={() => {}} />
-      <SVGComponent part="cdflatrotatablemirror" />
-      <SVGComponent part="samplecompartment" />
-      <SVGComponent part="mct" />
-      <SVGComponent part="lnsb" />
-      <SVGComponent part="cdflatrotatablemirror2" />
-      <SVGComponent part="fixedcornercube" />
-      <SVGComponent part="fixedmirror" />
-      <SVGComponent part="laser" />
-      <SVGComponent part="movablecornercube" />
-      <SVGComponent part="parabolicmirrorhole" />
-      <SVGComponent part="parabolicmirrorhole2" />
+      <div id="instument">
+        <SVGComponent part="laserbeams" click={() => {}} />
+        <SVGComponent part="sourcebox" click={() => {}} />
+        <SVGComponent part="aperturewheel" />
+        <SVGComponent part="flatrotatablemirror" />
+        <SVGComponent part="globar" />
+        <SVGComponent part="parabolicmirror" />
+        <SVGComponent part="tungsten" />
+        <SVGComponent part="detectionchamber" click={() => {}} />
+        <SVGComponent part="cdflatrotatablemirror" />
+        <SVGComponent part="samplecompartment" />
+        <SVGComponent part="mct" />
+        <SVGComponent part="lnsb" />
+        <SVGComponent part="cdflatrotatablemirror2" />
+        <SVGComponent part="fixedcornercube" />
+        <SVGComponent part="fixedmirror" />
+        <SVGComponent part="laser" />
+        <SVGComponent part="movablecornercube" />
+        <SVGComponent part="parabolicmirrorhole" />
+        <SVGComponent part="parabolicmirrorhole2" />
+      </div>
+
+      <div id="table">
+        <Electronics />
+      </div>
     </div>
   );
 }
