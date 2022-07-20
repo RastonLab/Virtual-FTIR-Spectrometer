11d1e2e248588907af90010c4b03d658c116947b
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-20 09:39:25 -0400

    All Resolution MUI Dropdown

--- src/components/inputs/Resolution.jsx
+++ src/components/inputs/Resolution.jsx
@@ -1,22 +1,34 @@
+import { MenuItem, Select, InputLabel} from "@mui/material";
 import React from "react";
 
 function Resolution({ params, setParams }) {
   return (
     <div className="input">
       <label htmlFor="Resolution">Resolution</label>
-      <select
-        id="database"
-        defaultValue={params.database}
-        onChange={(e) => setParams({ ...params, database: e.target.value })}
+      <Select
+      value={params}
+      onChange={(e) => setParams(e.target.value)}
       >
-        <option value="1">1</option>
-        <option value="0.5">0.5</option>
-        <option value="0.25">0.25</option>
-        <option value="0.125">0.125</option>
-        <option value="0.0625">0.0625</option>
-      </select>
+        <MenuItem value="1">1</MenuItem>
+        <MenuItem value="0.5">0.5</MenuItem>
+        <MenuItem value="0.25">0.25</MenuItem>
+        <MenuItem value="0.125">0.125</MenuItem>
+        <MenuItem value="0.0625">0.0625</MenuItem>
+      </Select>
     </div>
   );
 }
 
 export default Resolution;
+
+{/* <select
+id="database"
+defaultValue={params}
+onChange={(e) => setParams(e.target.value )}
+>
+<option value="1">1</option>
+<option value="0.5">0.5</option>
+<option value="0.25">0.25</option>
+<option value="0.125">0.125</option>
+<option value="0.0625">0.0625</option>
+</select> */}
