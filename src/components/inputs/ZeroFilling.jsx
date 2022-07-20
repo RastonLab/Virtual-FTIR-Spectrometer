f5d78fa072995cb7398434357e5ff8ff6741a58c
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-20 10:08:42 -0400

    ZeroFilling MUI Input

--- src/components/inputs/ZeroFilling.jsx
+++ src/components/inputs/ZeroFilling.jsx
@@ -1,18 +1,19 @@
+import { MenuItem, Select } from "@mui/material";
 import React from "react";
 
 function ZeroFilling ({ params, setParams }) {
   return (
     <div className="input">
       <label htmlFor="ZeroFilling">Zero Filling</label>
-      <select
+      <Select
         id="Zero Filling"
         defaultValue={params}
         onChange={(e) => setParams(e.target.value)}
-      >
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-      </select>
+      >        
+        <MenuItem value="0">0</MenuItem>
+        <MenuItem value="1">1</MenuItem>
+        <MenuItem value="2">2</MenuItem>
+      </Select>
     </div>
   );
 }
