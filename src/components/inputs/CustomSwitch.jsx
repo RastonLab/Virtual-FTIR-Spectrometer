1219ef2224f94f884080bffc1e12c53a9cda2e0e
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-21 09:10:57 -0400

    Change Switch Color

--- src/components/inputs/CustomSwitch.jsx
+++ src/components/inputs/CustomSwitch.jsx
@@ -1,19 +1,15 @@
 import { styled } from "@mui/material";
-import { purple, yellow, blue } from "@mui/material/colors";
+import { purple, pink, blue } from "@mui/material/colors";
 import { Switch } from "@mui/material";
 
 export const CustomSwitch = styled(Switch)(({ theme }) => ({
     '& .MuiSwitch-switchBase': {
         '&.Mui-checked': {
-            color: purple[600],
+            color: pink[400],
             '& + .MuiSwitch-track': {
-                backgroundColor: purple[500]
+                backgroundColor: pink[300]
             }
         },
-        // color: yellow[700],
-        // '& + .MuiSwitch-track': {
-        //     backgroundColor: yellow[600]
-        // }
         color: blue[700],
         '& + .MuiSwitch-track': {
             backgroundColor: blue[600]
