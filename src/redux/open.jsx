48712894a31c990b7163cb87790495b05def1eb0
Brennaser <sermanbc@dukes.jmu.edu>
2022-07-22 13:43:39 -0400

    Open File Works

--- src/redux/open.jsx
+++ src/redux/open.jsx
@@ -1,35 +1,47 @@
 import { useState } from "react";
+import "../style/Open.css";
+
+// https://dev.to/pankod/how-to-import-csv-file-with-react-4pj2
 
 export const Open = () => {
     
     const [selectedFile, setSelectedFile] = useState();
     const [isSelected, setIsSelected] = useState(false);
+    const [data, setData] = useState();
+
+    const filereader = new FileReader();
 
     const changeHandler = (event) => {
 		setSelectedFile(event.target.files[0]);
-        console.log(event.target.files);
 		setIsSelected(true);
 	};
 
-	const handleSubmission = () => {
-        if (isSelected) {
-            fetch(selectedFile)
-                .then((response) => response.text())
-                .then((result) => {
-                    console.log('Success:', result);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
-        }
+	const handleSubmission = (event) => {
+        event.preventDefault();
+        console.log(data);
 
-	};
+        if(isSelected) {
+            filereader.onload = function (e) {
+                setData(e.target.result);
+            };
+            filereader.readAsText(selectedFile);
+        }
+        console.log(data);
+
+    }
 
     return(
-        <div>
-            <input type='file' name='file' onChange={changeHandler} />
-            <button onClick={handleSubmission}>Upload</button>
-            <p className="content" ></p>
+        <div className="open">
+            <div className="row">
+                <label className=".custom-file-upload button">
+                    Select a File
+                    <input type='file' name='file' onChange={changeHandler} />
+                </label>
+                <button className="button" onClick={(e) => handleSubmission(e)}>Upload</button>
+            </div>
+            <div className="row file">
+                <p className="content" >{data}</p>
+            </div>
         </div>
     );
 }
