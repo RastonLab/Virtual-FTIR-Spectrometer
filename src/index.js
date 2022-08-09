4d18ed5453cd1bbbf1eed17812475b16a4de39f0
VanFossen <nvanfossen@gmail.com>
2022-08-09 16:01:45 -0400

    sentry.io

--- src/index.js
+++ src/index.js
@@ -15,17 +15,17 @@ import "./index.css";
 import store from "./redux/store";
 
 // -------------------- sentry.io tracking --------------------
-// import * as Sentry from "@sentry/react";
-// import { BrowserTracing } from "@sentry/tracing";
-// Sentry.init({
-//   dsn: "https://e7a614a92d3f4f929ae8e3de0fcebff7@o1310725.ingest.sentry.io/6558449",
-//   integrations: [new BrowserTracing()],
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+Sentry.init({
+  dsn: "https://e7a614a92d3f4f929ae8e3de0fcebff7@o1310725.ingest.sentry.io/6558449",
+  integrations: [new BrowserTracing()],
 
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for performance monitoring.
-//   // We recommend adjusting this value in production
-//   tracesSampleRate: 1.0,
-// });
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 // -------------------- react router --------------------
 const root = ReactDOM.createRoot(document.getElementById("root"));
