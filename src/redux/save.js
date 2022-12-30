6d7f76acee680cff484e544cb235e74621fdf44b
Brennaser <sermanbc@dukes.jmu.edu>
2022-12-30 18:44:32 -0500

    restored working version

--- src/redux/save.js
+++ src/redux/save.js
@@ -9,36 +9,19 @@ export const Save = () => {
   }
 
   const params = state.params;
-  const pData = state.processedData;
-  // const bgData = state.backgroundData;
-
+  const xVals = state.processedData;
+  console.log(xVals);
+  // const yVals = state.processedData.y;
 
 
   let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
 
-  for(let i = 0; i < pData.length; i++) {
-    // console.log(pData[i]);
-    data += `${pData[i]}\n`
+  for(let i = 0; i < xVals.length; i++) {
+
+    data += `${xVals[i]}\n`
   }
 
 
   const blob = new Blob(new Array([data]), { type: "application/csv" });
   FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-spectrum.csv`)
-  
-
-  // if (bgData != null) {
-
-  //   let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
-
-  //   for(let i = 0; i < bgData.length; i++) {
-  
-  //     data += `${bgData[i]}\n`
-  //   }
-  
-  
-  //   const blob = new Blob(new Array([data]), { type: "application/csv" });
-  //   FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-background-spectrum.csv`)
-  // }
-
-
 };
