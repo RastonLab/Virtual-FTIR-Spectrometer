74e729b13911cde2f6c77b0d9db8120aa1760cc7
Brennaser <sermanbc@dukes.jmu.edu>
2022-12-30 17:40:43 -0500

    precessedcData

--- src/redux/save.js
+++ src/redux/save.js
@@ -9,9 +9,9 @@ export const Save = () => {
   }
 
   const params = state.params;
-  const yVals = state.data.y;
+  const yVals = state.processedData.y;
   console.log("just fine\n");
-  const xVals = state.data.x;
+  const xVals = state.processedData.x;
 
 
   let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
