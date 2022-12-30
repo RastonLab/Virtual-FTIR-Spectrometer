dedc198cfe1e15391528da54bfb388cb92bca231
Brennaser <sermanbc@dukes.jmu.edu>
2022-12-30 18:23:47 -0500

    finding why the saved files have no data

--- src/redux/save.js
+++ src/redux/save.js
@@ -17,7 +17,7 @@ export const Save = () => {
     let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
 
     for(let i = 0; i < pData.length; i++) {
-  
+      console.log(pData[i]);
       data += `${pData[i]}\n`
     }
   
