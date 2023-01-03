772b048be222718a42d685d3e7cef29ec94c1dae
Brennaser <sermanbc@dukes.jmu.edu>
2023-01-03 14:22:46 -0500

    Save Background Data

--- src/redux/save.js
+++ src/redux/save.js
@@ -9,20 +9,31 @@ export const Save = () => {
   }
 
   const params = state.params;
-  const xVals = state.processedData.x;
-  // console.log(xVals);
-  const yVals = state.processedData.y;
 
 
-  let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
-  // console.log("start loop");
-  for(let i = 0; i < xVals.length; i++) {
-    // console.log('loop');
-    console.log(`${xVals[i]} ${yVals[i]}\n`);
-    data += `${xVals[i]} ${yVals[i]}\n`;
+  if (state.processedData != null) {
+    const xVals = state.processedData.x;
+    const yVals = state.processedData.y;
+    let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
+    for(let i = 0; i < xVals.length; i++) {
+      data += `${xVals[i]} ${yVals[i]}\n`;
+    }
+  
+    const blob = new Blob(new Array([data]), { type: "application/csv" });
+    FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-spectrum.csv`)
   }
-  // console.log("exit loop");
 
-  const blob = new Blob(new Array([data]), { type: "application/csv" });
-  FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-spectrum.csv`)
+  if (state.backgroundData != null) {
+    const xVals = state.backgroundData.x;
+    const yVals = state.backgroundData.y;
+    let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
+    for(let i = 0; i < xVals.length; i++) {
+      data += `${xVals[i]} ${yVals[i]}\n`;
+    }
+  
+    const blob = new Blob(new Array([data]), { type: "application/csv" });
+    FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-background-spectrum.csv`)
+  }
+
+
 };
