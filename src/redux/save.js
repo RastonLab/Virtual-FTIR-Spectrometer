90f13deb84b68c0d8cf52c68f58b26d99120eecf
Brennaser <sermanbc@dukes.jmu.edu>
2023-01-02 19:54:44 -0500

    print all data (both x and y

--- src/redux/save.js
+++ src/redux/save.js
@@ -10,18 +10,18 @@ export const Save = () => {
 
   const params = state.params;
   const xVals = state.processedData.x;
-  console.log(xVals);
-  // const yVals = state.processedData.y;
+  // console.log(xVals);
+  const yVals = state.processedData.y;
 
 
   let data = `# Min Wavenumber: ${params.minWave} Max Wavenumber: ${params.maxWave} Molecule: ${params.molecule} Pressure: ${params.pressure} Resolution: ${params.resolution} Number of Scans: ${params.numScan} Zero Fill: ${params.zeroFill} Source: ${params.source} Beamsplitter: ${params.beamsplitter} Cell Window: ${params.cellWindow} Detector: ${params.detector} \n`;
-  console.log("start loop");
+  // console.log("start loop");
   for(let i = 0; i < xVals.length; i++) {
-    console.log('loop');
-    console.log(`${xVals[i]}\n`);
-    data += `${xVals[i]}\n`;
+    // console.log('loop');
+    console.log(`${xVals[i]} ${yVals[i]}\n`);
+    data += `${xVals[i]} ${yVals[i]}\n`;
   }
-  console.log("exit loop");
+  // console.log("exit loop");
 
   const blob = new Blob(new Array([data]), { type: "application/csv" });
   FileSaver.saveAs(blob, `${params.minWave}-${params.maxWave}-spectrum.csv`)
