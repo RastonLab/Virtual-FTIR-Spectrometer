daeaf6ddb8c617294272784460035c26adb8493b
Brennaser <sermanbc@dukes.jmu.edu>
2023-01-06 14:11:34 -0500

    Basic Tab Test | Spectrum Window

--- src/routes/SpectrumWindow.jsx
+++ src/routes/SpectrumWindow.jsx
@@ -7,28 +7,63 @@ import BackgroundPlotly from "../components/BackgroundPlotly";
 
 import "../style/routes/SpectrumWindow.css";
 
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 export default function SpectrumWindow() {
   const storedProcessedData = useSelector((state) => state.processedData);
   const storedBackgroundData = useSelector((state) => state.backgroundData);
+  const [tabValue, setValue] = React.useState(0);
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
 
   return (
-    <div id="spectrum-window">
-      <div id="spectrum">
+    <Box>
+      <Box sx={{ borederBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabValue} onChange={handleChange} aria-label="Spectrum Window Selection">
+          <Tab label="Processed Spectrum" /> {/*TODO Check aria labels */}
+          <Tab label="Background Spectrum" />
+          <Tab label="Both Spectra" disabled />
+        </Tabs>
+      </Box>
+      <TabPanel value={tabValue} index={0}>
         {storedProcessedData ? (
           <ProcessedPlotly />
         ) : (
           <p>Please generate a processed spectrum and return here</p>
         )}
-      </div>
-
-      <div id="spectrum">
+      </TabPanel>
+      <TabPanel value={tabValue} index={1}>
         {storedBackgroundData ? (
-          <BackgroundPlotly />
-        ) : (
-          <p>Please generate a background spectrum and return here</p>
-        )}
-      </div>
-      <Outlet />
-    </div>
+            <BackgroundPlotly />
+          ) : (
+            <p>Please generate a background spectrum and return here</p>
+          )}
+      </TabPanel>
+    </Box>
   );
+
+  // return (
+  //   <div id="spectrum-window">
+  //     <div id="spectrum">
+  //       {storedProcessedData ? (
+  //         <ProcessedPlotly />
+  //       ) : (
+  //         <p>Please generate a processed spectrum and return here</p>
+  //       )}
+  //     </div>
+
+  //     <div id="spectrum">
+        // {storedBackgroundData ? (
+        //   <BackgroundPlotly />
+        // ) : (
+        //   <p>Please generate a background spectrum and return here</p>
+        // )}
+  //     </div>
+  //     <Outlet />
+  //   </div>
+  // );
 }
