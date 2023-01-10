b3ec2f61fdadd6f4619dea64bac5f49e21dcc22d
Brennaser <sermanbc@dukes.jmu.edu>
2023-01-10 16:28:36 -0500

    Add Default Value

--- src/components/inputs/AirVac.jsx
+++ src/components/inputs/AirVac.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 // this input component sets the source temperature to globar (1700 K) or tungsten (3100 K)
 export default function AirVac({ params, setParams, pressure, setPressure }) {
 
-    const [oldPressure, setOldPressure] = useState();
+    const [oldPressure, setOldPressure] = useState(pressure);
 
     const changePressure = () => {
         params === false ? setParams(true) : setParams(false); // air is true
