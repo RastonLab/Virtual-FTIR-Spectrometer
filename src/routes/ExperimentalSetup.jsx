2067cb3c52d9cb3d7b58e31b87481fd3c7921f42
Brennaser <sermanbc@dukes.jmu.edu>
2023-05-24 13:10:45 -0400

    Local host

--- src/routes/ExperimentalSetup.jsx
+++ src/routes/ExperimentalSetup.jsx
@@ -112,8 +112,8 @@ const ExperimentalSetup = (props, ref) => {
                 detector,
                 airVac
               }}
-              // fetchURL={"http://localhost:5000/spectrum"}
-              fetchURL={"https://api.ftir.rastonlab.org/spectrum"}
+              fetchURL={"http://localhost:5000/spectrum"}
+              // fetchURL={"https://api.ftir.rastonlab.org/spectrum"}
               buttonText="Generate Processed Spectrum"
             />
             <Fetch
@@ -132,8 +132,8 @@ const ExperimentalSetup = (props, ref) => {
                 detector,
                 airVac
               }}
-              // fetchURL={"http://localhost:5000/background"}
-              fetchURL={"https://api.ftir.rastonlab.org/background"}
+              fetchURL={"http://localhost:5000/background"}
+              // fetchURL={"https://api.ftir.rastonlab.org/background"}
               buttonText={"Generate Background Sample"}
             />
           </div>
