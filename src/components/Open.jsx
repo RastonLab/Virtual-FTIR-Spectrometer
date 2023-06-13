e60d5db4577b547226af9940031d1b6eb9d4c31d
Brennaser <sermanbc@dukes.jmu.edu>
2023-06-13 11:25:04 -0400

    Begining rework, basic reading and store imports

--- src/components/Open.jsx
+++ src/components/Open.jsx
@@ -7,6 +7,9 @@ import { useDispatch } from "react-redux";
 import { deactivateError } from "../features/errorSlice";
 import { updateBackgroundData } from "../features/backgroundDataSlice";
 import { updateSpectrumData } from "../features/spectrumDataSlice";
+import { updateBeamsplitter, updateDetector, updateMedium, updateMolecule,
+  updatePressure, updateResolution, updateScan, updateSource, updateWaveMax,
+  updateWaveMin, updateWindow, updateZeroFill } from "../features/parameterSlice";
 
 // style
 import "../style/components/Open.css";
@@ -34,55 +37,62 @@ export const Open = () => {
   };
 
   const handleSubmission = () => {
-    const xData = [];
-    const yData = [];
+
     let rawData = data;
 
+    let index = rawData.indexOf("\n");
+    let line = rawData.substring(0, index);
+
+      // Gathers Parameters
+      const parameters = [];
+      while (line.indexOf(":") > 0) {
+        let paramStart = line.indexOf(":") + 2; // The Additional 2 accounts for the colon inself and the following space
+        line = line.substring(paramStart);
+        let paramEnd = line.indexOf(" ");
+        let param = line.substring(0, paramEnd);
+        line = line.substring(paramEnd + 1);
+        parameters.push(param);
+      }
+
+      for (let i = 0; i < parameters.length; i++){
+        console.log(parameters[i]);
+      }
+
+      // dispatch(
+      //   storeParams({
+      //     minWave: parameters[0],
+      //     maxWave: parameters[1],
+      //     molecule: parameters[2],
+      //     pressure: parameters[3],
+      //     resolution: parameters[4],
+      //     numScan: parameters[5],
+      //     zeroFill: parameters[6],
+      //     source: parameters[7],
+      //     beamsplitter: parameters[8],
+      //     cellWindow: parameters[9],
+      //     detector: parameters[10],
+      //   })
+      // );
+
+    const xData = [];
+    const yData = [];
+
     while (rawData !== "") {
+
       let index = rawData.indexOf("\n");
       let line = rawData.substring(0, index);
 
-      if (line.charAt(0) !== "#") {
-        let comma = line.indexOf(",");
-        let x = line.substring(0, comma);
-        let y = line.substring(comma + 1);
+      let comma = line.indexOf(",");
+      let x = line.substring(0, comma);
+      let y = line.substring(comma + 1);
 
-        xData.push(Number(x));
-        yData.push(Number(y));
-        // } else {
-        //   const parameters = [];
-        //   while (line.indexOf(":") > 0) {
-        //     let paramStart = line.indexOf(":") + 2; // The Additional 2 accounts for the colon inself and the following space
-        //     line = line.substring(paramStart);
-        //     let paramEnd = line.indexOf(" ");
-        //     let param = line.substring(0, paramEnd);
-        //     line = line.substring(paramEnd + 1);
-        //     parameters.push(param);
-        //   }
+      xData.push(Number(x));
+      yData.push(Number(y));
 
-        //   for (let i = 0; i < parameters.length; i++){
-        //     console.log(parameters[i]);
-        //   }
-
-        //   dispatch(
-        //     storeParams({
-        //       minWave: parameters[0],
-        //       maxWave: parameters[1],
-        //       molecule: parameters[2],
-        //       pressure: parameters[3],
-        //       resolution: parameters[4],
-        //       numScan: parameters[5],
-        //       zeroFill: parameters[6],
-        //       source: parameters[7],
-        //       beamsplitter: parameters[8],
-        //       cellWindow: parameters[9],
-        //       detector: parameters[10],
-        //     })
-        //   );
-      }
       rawData = rawData.substring(index + 1);
     }
 
+    // NOTE: Handled differently now
     if (filename.includes("background")) {
       dispatch(updateBackgroundData({ x: xData, y: yData }));
     } else {
