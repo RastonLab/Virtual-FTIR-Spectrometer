eb55003eed610a33d8f570797e1391712e67149b
Nicholas VanFossen <35882058+VanFossen@users.noreply.github.com>
2023-07-19 11:30:33 -0400

    fix devtool warning about missing unique key props

--- src/App.js
+++ src/App.js
@@ -13,17 +13,21 @@ import { Outlet, Link } from "react-router-dom";
 // style
 import "./style/App.css";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 import DevMode from "./components/DevMode";
-import { Accordion, AccordionDetails, AccordionSummary, Drawer } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Drawer,
+} from "@mui/material";
 
 export default function App() {
-
   const [expanded, setExpanded] = useState("");
   const [drawer, setDrawer] = useState(false);
 
@@ -39,20 +43,20 @@ export default function App() {
     setDrawer(false);
   };
 
-  return (  
-    <div>  
+  return (
+    <div>
       <AppBar className="nav-area" position="static">
-        <Container maxWidth="xl" sx={{paddingLeft: {xs: 0}}}>
+        <Container maxWidth="xl" sx={{ paddingLeft: { xs: 0 } }}>
           <Toolbar
-            sx={{  
+            sx={{
               alignItems: "flex-start",
               padding: "10px",
               margin: "0",
-              justifySelf: "left"
+              justifySelf: "left",
             }}
           >
             {/* Start Small Menu */}
-            <Box sx={{ flexGrow: 0.1, display: { xs: 'flex', lg: 'none' } }}>
+            <Box sx={{ flexGrow: 0.1, display: { xs: "flex", lg: "none" } }}>
               <IconButton
                 size="large"
                 aria-label="Menu and Navigation"
@@ -61,7 +65,7 @@ export default function App() {
                 onClick={handleOpenNavMenu}
                 color="inherit"
               >
-              <MenuIcon />
+                <MenuIcon />
               </IconButton>
               <Drawer
                 open={drawer}
@@ -69,62 +73,70 @@ export default function App() {
                 onClose={handleCloseNavMenu}
               >
                 <Box>
-                {menuItems.map((page) => (
-                  <Accordion
-                    square
-                    expanded={expanded === page.label}
-                    onChange={handleChange(page.label)}
-                    onMouseLeave={handleChange("")}
-                    disableGutters
-                  >
-                    <AccordionSummary
-                      className="menu-items"
+                  {menuItems.map((page) => (
+                    <Accordion
+                      key={page.label}
+                      square
+                      expanded={expanded === page.label}
+                      onChange={handleChange(page.label)}
+                      onMouseLeave={handleChange("")}
+                      disableGutters
                     >
-                      {page.label}
-                    </AccordionSummary>
-                    <AccordionDetails
-                      className="dropdown-items"
-                    >
-                      {page.submenu.map((submenu) => {
-                        if (submenu.button) {
-                          return (
+                      <AccordionSummary className="menu-items">
+                        {page.label}
+                      </AccordionSummary>
+                      <AccordionDetails className="dropdown-items">
+                        {page.submenu.map((submenu) => {
+                          if (submenu.button) {
+                            return (
                               <Popup
+                                key={submenu.label}
                                 label={submenu.label}
                                 title={submenu.title}
                                 text={submenu.text}
                               />
-                          );
-                        } else if (submenu.link) {
-                          return (
-                            <p>
-                              <Link to={submenu.link ?? "#"} onClick={submenu.action}>
-                                {submenu.label}
-                              </Link>
-                            </p>
-                          );
-                        } else {
-                          return (<ul>{submenu.component}</ul>)
-                        }
-                      })}
-                    </AccordionDetails>
-                  </Accordion>
-                ))}
+                            );
+                          } else if (submenu.link) {
+                            return (
+                              <p key={submenu.link}>
+                                <Link
+                                  to={submenu.link ?? "#"}
+                                  onClick={submenu.action}
+                                >
+                                  {submenu.label}
+                                </Link>
+                              </p>
+                            );
+                          } else {
+                            return (
+                              <ul key={submenu.label}>{submenu.component}</ul>
+                            );
+                          }
+                        })}
+                      </AccordionDetails>
+                    </Accordion>
+                  ))}
                 </Box>
               </Drawer>
             </Box>
             {/* End Small Menu */}
 
             {/* Start Logo and Title */}
-            <a className="logolink" target="_blank" rel="noreferrer" href="https://www.rastonlab.org/">
+            <a
+              className="logolink"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.rastonlab.org/"
+            >
               <RLLogo width={50} height={50} viewBox="0 0 100 100" />
             </a>
-            <Link className="logo" to="/" >
-              <span className="red"   >F</span>
+            <Link className="logo" to="/">
+              <span className="red">F</span>
               <span className="orange">T</span>
               <span className="yellow">I</span>
-              <span className="green" >R</span>
-              <span className="teal"  >-</span>
-              <span className="blue"  >S</span>
+              <span className="green">R</span>
+              <span className="teal">-</span>
+              <span className="blue">S</span>
               <span className="indigo">I</span>
               <span className="purple">S</span>
             </Link>
@@ -132,55 +144,56 @@ export default function App() {
 
             {/* Start Full Sized Menu */}
             {menuItems.map((page) => (
-            <Accordion
-              square
-              expanded={expanded === page.label}
-              onChange={handleChange(page.label)}
-              onMouseLeave={handleChange("")}
-              sx={{
-                display: { xs: 'none', lg: 'block' },
-                boxShadow: "none"
-              }}
-              disableGutters
-            >
-              <AccordionSummary
-                className="menu-items"
+              <Accordion
+                key={page.label}
+                square
+                expanded={expanded === page.label}
+                onChange={handleChange(page.label)}
+                onMouseLeave={handleChange("")}
+                sx={{
+                  display: { xs: "none", lg: "block" },
+                  boxShadow: "none",
+                }}
+                disableGutters
               >
-                {page.label}
-              </AccordionSummary>
-              <AccordionDetails
-                className="dropdown-items"
-              >
-                {page.submenu.map((submenu) => {
-                  if (submenu.button) {
-                    return (
+                <AccordionSummary className="menu-items">
+                  {page.label}
+                </AccordionSummary>
+                <AccordionDetails className="dropdown-items">
+                  {page.submenu.map((submenu) => {
+                    if (submenu.button) {
+                      return (
                         <Popup
+                          key={submenu.label}
                           label={submenu.label}
                           title={submenu.title}
                           text={submenu.text}
                         />
-                    );
-                  } else if (submenu.link) {
-                    return (
-                      <p>
-                        <Link to={submenu.link ?? "#"} onClick={submenu.action}>
-                          {submenu.label}
-                        </Link>
-                      </p>
-                    );
-                  } else {
-                    return (<ul>{submenu.component}</ul>)
-                  }
-                })}
-              </AccordionDetails>
-            </Accordion>
+                      );
+                    } else if (submenu.link) {
+                      return (
+                        <p key={submenu.link}>
+                          <Link
+                            to={submenu.link ?? "#"}
+                            onClick={submenu.action}
+                          >
+                            {submenu.label}
+                          </Link>
+                        </p>
+                      );
+                    } else {
+                      return <ul key={submenu.label}>{submenu.component}</ul>;
+                    }
+                  })}
+                </AccordionDetails>
+              </Accordion>
             ))}
             {/* End Full Sized Menu */}
             <DevMode />
           </Toolbar>
         </Container>
       </AppBar>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
