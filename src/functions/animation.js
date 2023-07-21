f5e11e417a579e7616546df2039f4b2987e7096e
Nicholas VanFossen <35882058+VanFossen@users.noreply.github.com>
2023-07-21 11:22:42 -0400

    animation working, only chromium

--- src/functions/animation.js
+++ src/functions/animation.js
@@ -1,29 +1,66 @@
 export function animateCornerCube() {
   const mcc = document.getElementById("movable-corner-cube");
-  const rayTop = document.getElementById("rect14491-7-9-2-3-5");
-
-  console.log(rayTop);
+  const rayTop = document.getElementById("ray-top");
+  const rayMiddle = document.getElementById("ray-middle");
+  const rayBottom = document.getElementById("ray-bottom");
+  const laser = document.getElementById("rect6675");
 
   if (mcc) {
+    // moveable corner cube
     mcc.setAttribute(
       "transform",
       "rotate(135 900.985 1021.051) translate(-800, -800)"
     );
+
+    // top ray
     rayTop.setAttribute(
       "d",
-      "m 953.417-3200.486 94.391-91.072v1920.323l-94.391 95.491z"
+      "m953.417-3217.12 94.391-91.071v1860.955l-94.391 95.49z"
+    );
+
+    // middle ray
+    rayMiddle.setAttribute("transform", "rotate(90) translate (0, -1130)");
+
+    // bottom ray
+    rayBottom.setAttribute(
+      "d",
+      "m1164.195-3310.854 94.356 95.49v2632.447l-94.356 89.932z"
+    );
+
+    // laser
+    laser.setAttribute(
+      "d",
+      "M1406.494 991.284v10H3190.73v209.254H344.494v10H3200.73V991.284Z"
     );
 
     mcc.addEventListener(
       "transitionend",
       () => {
+        // moveable corner cube
         mcc.setAttribute(
           "transform",
           "rotate(135 900.985 1021.051) translate(0, 0)"
         );
+
+        // top ray
         rayTop.setAttribute(
           "d",
-          "m953.417-2026.826 94.391-91.072v670.662l-94.391 95.49z"
+          "m953.417-2026.826 94.391-93.734v673.324l-94.391 95.49z"
+        );
+
+        // middle ray
+        rayMiddle.setAttribute("transform", "rotate(90) translate (0, 0)");
+
+        // bottom ray
+        rayBottom.setAttribute(
+          "d",
+          "m1164.195-2120.56 94.356 93.733v1443.91l-94.356 89.932z"
+        );
+
+        // laser
+        laser.setAttribute(
+          "d",
+          "M1406.494 991.284v10h651.235v209.254H344.494v10H2067.73V991.284z"
         );
       },
       { once: true }
